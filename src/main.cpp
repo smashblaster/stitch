c9ca2d59e1ab04431051ef17eb8800a0bea893e8
Ross Hadden <rosshadden@gmail.com>
2017-04-09 16:20:29 -0400

    Prod mode?

--- src/main.cpp
+++ src/main.cpp
@@ -64,23 +64,25 @@ void remap(Gamecube_Report_t state, Gamecube_Data_t *data) {
 		(*data).report.l = 1;
 	}
 
-	// Map dright => toggle between vanilla / dolphin
-	if (state.dright == 1) {
-		(*data).report.dright = 0;
-		if (maxDashBuffer == maxDashBufferVanilla) {
-			maxDashBuffer = maxDashBufferDolphin;
-			digitalWrite(LED_BUILTIN, HIGH);
-		} else {
-			maxDashBuffer = maxDashBufferVanilla;
-			digitalWrite(LED_BUILTIN, LOW);
-		}
-	}
+	// // Map dright => toggle between vanilla / dolphin
+	// if (state.dright == 1) {
+	// 	(*data).report.dright = 0;
+	// 	if (maxDashBuffer == maxDashBufferVanilla) {
+	// 		maxDashBuffer = maxDashBufferDolphin;
+	// 		digitalWrite(LED_BUILTIN, HIGH);
+	// 	} else {
+	// 		maxDashBuffer = maxDashBufferVanilla;
+	// 		digitalWrite(LED_BUILTIN, LOW);
+	// 	}
+	// }
 
-	if (state.dup == 1) {
-		(*data).report.dup = 0;
-		(*data).report.xAxis = 138;
-		(*data).report.yAxis = 80;
-	}
+	// if (state.dup == 1) {
+	// 	(*data).report.dup = 0;
+	// 	(*data).report.xAxis = 138;
+	// 	(*data).report.yAxis = 80;
+	// 	// (*data).report.xAxis = 55;
+	// 	// (*data).report.yAxis = 51;
+	// }
 }
 
 void dashback(Gamecube_Report_t state, Gamecube_Data_t *data) {
@@ -139,7 +141,7 @@ void loop() {
 	}
 
 	if (isPassthrough == false) {
-		debug(state, &data);
+		// debug(state, &data);
 		remap(state, &data);
 		dashback(state, &data);
 	}
