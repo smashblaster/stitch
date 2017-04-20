98955c30c6a2d06661eff613bf41b81fd5bc5a48
Ross Hadden <rosshadden@gmail.com>
2017-04-19 22:25:09 -0400

    Made context a class, and moved button fns to it.

--- src/context.cpp
+++ src/context.cpp
@@ -4,15 +4,61 @@
 #ifndef CONTEXT
 #define CONTEXT
 
-struct Context {
-	Gamecube_Data_t data;
-	Gamecube_Report_t state;
-	bool debug = false;
-	bool enabled = true;
-	bool init = false;
-	bool rumble = false;
-	std::set<std::string> pressed;
-	std::set<std::string> released;
+class Context {
+	public:
+		Gamecube_Data_t data;
+		Gamecube_Report_t state;
+		bool debug = false;
+		bool enabled = true;
+		bool init = false;
+		bool rumble = false;
+		std::set<std::string> pressed;
+		std::set<std::string> released;
+
+		~Context() {}
+
+		uint8_t getButton(std::string button) {
+			if (button == "a") return state.a;
+			if (button == "b") return state.b;
+			if (button == "x") return state.x;
+			if (button == "y") return state.y;
+			if (button == "l") return state.l;
+			if (button == "r") return state.r;
+			if (button == "z") return state.z;
+			if (button == "start") return state.start;
+			if (button == "ddown") return state.ddown;
+			if (button == "dleft") return state.dleft;
+			if (button == "dright") return state.dright;
+			if (button == "dup") return state.dup;
+			if (button == "cxAxis") return state.cxAxis;
+			if (button == "cyAxis") return state.cyAxis;
+			if (button == "left") return state.left;
+			if (button == "right") return state.right;
+			if (button == "xAxis") return state.xAxis;
+			if (button == "yAxis") return state.yAxis;
+			return 0;
+		}
+
+		void setButton(std::string button, uint8_t value) {
+			if (button == "a") data.report.a = value;
+			if (button == "b") data.report.b = value;
+			if (button == "x") data.report.x = value;
+			if (button == "y") data.report.y = value;
+			if (button == "l") data.report.l = value;
+			if (button == "r") data.report.r = value;
+			if (button == "z") data.report.z = value;
+			if (button == "start") data.report.start = value;
+			if (button == "ddown") data.report.ddown = value;
+			if (button == "dleft") data.report.dleft = value;
+			if (button == "dright") data.report.dright = value;
+			if (button == "dup") data.report.dup = value;
+			if (button == "cxAxis") data.report.cxAxis = value;
+			if (button == "cyAxis") data.report.cyAxis = value;
+			if (button == "left") data.report.left = value;
+			if (button == "right") data.report.right = value;
+			if (button == "xAxis") data.report.xAxis = value;
+			if (button == "yAxis") data.report.yAxis = value;
+		}
 };
 
 #endif
--- src/modules/map.cpp
+++ src/modules/map.cpp
@@ -18,64 +18,15 @@ class Map: public Module {
 			ctx->released.clear();
 
 			for (auto &button : buttons) {
-				uint8_t value = getButton(ctx, button);
+				uint8_t value = ctx->getButton(button);
 				if (value == 1 && !buttonState[button]) {
 					ctx->pressed.insert(button);
 				} else if (value == 0 && buttonState[button]) {
 					ctx->released.insert(button);
 				}
 
+				ctx->setButton(button, value);
 				buttonState[button] = value == 1;
 			}
-
-			ctx->data.report.a = ctx->state.a;
-			ctx->data.report.b = ctx->state.b;
-			ctx->data.report.x = ctx->state.x;
-			ctx->data.report.y = ctx->state.y;
-			ctx->data.report.start = ctx->state.start;
-			ctx->data.report.dleft = ctx->state.dleft;
-			ctx->data.report.dright = ctx->state.dright;
-			ctx->data.report.ddown = ctx->state.ddown;
-			ctx->data.report.dup = ctx->state.dup;
-			ctx->data.report.z = ctx->state.z;
-			ctx->data.report.l = ctx->state.l;
-			ctx->data.report.r = ctx->state.r;
-			ctx->data.report.cxAxis = ctx->state.cxAxis;
-			ctx->data.report.cyAxis = ctx->state.cyAxis;
-			ctx->data.report.left = ctx->state.left;
-			ctx->data.report.right = ctx->state.right;
-			ctx->data.report.xAxis = ctx->state.xAxis;
-			ctx->data.report.yAxis = ctx->state.yAxis;
-		}
-
-		uint8_t getButton(Context *ctx, std::string button) {
-			if (button == "a") return ctx->state.a;
-			if (button == "b") return ctx->state.b;
-			if (button == "x") return ctx->state.x;
-			if (button == "y") return ctx->state.y;
-			if (button == "l") return ctx->state.l;
-			if (button == "r") return ctx->state.r;
-			if (button == "z") return ctx->state.z;
-			if (button == "start") return ctx->state.start;
-			if (button == "ddown") return ctx->state.ddown;
-			if (button == "dleft") return ctx->state.dleft;
-			if (button == "dright") return ctx->state.dright;
-			if (button == "dup") return ctx->state.dup;
-			return 0;
-		}
-
-		void setButton(Context *ctx, std::string button, uint8_t value) {
-			if (button == "a") ctx->data.report.a = value;
-			if (button == "b") ctx->data.report.b = value;
-			if (button == "x") ctx->data.report.x = value;
-			if (button == "y") ctx->data.report.y = value;
-			if (button == "l") ctx->data.report.l = value;
-			if (button == "r") ctx->data.report.r = value;
-			if (button == "z") ctx->data.report.z = value;
-			if (button == "start") ctx->data.report.start = value;
-			if (button == "ddown") ctx->data.report.ddown = value;
-			if (button == "dleft") ctx->data.report.dleft = value;
-			if (button == "dright") ctx->data.report.dright = value;
-			if (button == "dup") ctx->data.report.dup = value;
 		}
 };
