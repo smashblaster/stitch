b1d4592eedba3a640b8fd20068e7debad2b09cf9
Ross Hadden <rosshadden@gmail.com>
2017-05-09 00:57:43 -0400

    Added shield drop notches.

--- src/systems/angles.cpp
+++ src/systems/angles.cpp
@@ -3,11 +3,11 @@
 class Angles: public System {
 	using System::System;
 
-	// const float angleLeft = -164;
-	const float angleLeft = -163.4465;
+	// const float angleLeft = -163.4465;
+	const float angleLeft = -162;
 	const float angleLeftRad = angleLeft * M_PI / 180;
-	const float angleRight = -16.5;
-	// const float angleRight = -17.4180;
+	// const float angleRight = -16.5;
+	const float angleRight = -17.5;
 	const float angleRightRad = angleRight * M_PI / 180;
 	const int desiredRadius = 110;
 	const int angleThreshold = 12;
@@ -22,19 +22,32 @@ class Angles: public System {
 			float angle = getAngle();
 			float radius = getRadius();
 
-			// if (radius >= 98 && (angle - (angleLeft - angleThreshold)) <= ((angleLeft + angleThreshold) - (angleLeft - angleThreshold))) {
-			if (radius >= 90 && angleLeft - angleThreshold <= angle && angle <= angleLeft + angleThreshold) {
-				// Serial.println("left");
-				// ctx->set("xAxis", -98 + gridOffset);
-				// ctx->set("yAxis", -31 + gridOffset);
-				setAngle(angleLeftRad, desiredRadius);
-			// } else if (radius >= 98 && (angle - (angleRight - angleThreshold)) <= ((angleRight + angleThreshold) - (angleRight - angleThreshold))) {
-			} else if (radius >= 90 && angleRight - angleThreshold <= angle && angle <= angleRight + angleThreshold) {
-				// Serial.println("right");
-				// ctx->set("xAxis", 102 + gridOffset);
-				// ctx->set("yAxis", -33 + gridOffset);
-				setAngle(angleRightRad, desiredRadius);
+			// Wavedash
+			if (90 <= radius) {
+				// if (radius >= 98 && (angle - (angleLeft - angleThreshold)) <= ((angleLeft + angleThreshold) - (angleLeft - angleThreshold))) {
+				if (angleLeft - angleThreshold <= angle && angle <= angleLeft + angleThreshold) {
+					// Serial.println("left");
+					// ctx->set("xAxis", -98 + gridOffset);
+					// ctx->set("yAxis", -31 + gridOffset);
+					setAngle(angleLeftRad, desiredRadius);
+				// } else if (radius >= 98 && (angle - (angleRight - angleThreshold)) <= ((angleRight + angleThreshold) - (angleRight - angleThreshold))) {
+				} else if (angleRight - angleThreshold <= angle && angle <= angleRight + angleThreshold) {
+					// Serial.println("right");
+					// ctx->set("xAxis", 102 + gridOffset);
+					// ctx->set("yAxis", -33 + gridOffset);
+					setAngle(angleRightRad, desiredRadius);
+				}
+
+				// Shield drop
+				if (-135 - 4 <= angle && angle <= -135 + 4) {
+					setAngle(-135 * M_PI / 180, desiredRadius);
+				} else if (-40 - 4 <= angle && angle <= -40 + 4) {
+					setAngle(-43 * M_PI / 180, desiredRadius);
+				}
 			}
+
+			// Perfect shield drop
+			// setAngle(-134 * M_PI / 180, 75);
 		}
 
 		float getAngle() {
