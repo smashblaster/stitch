55686161b1b88f9465392e59e1c716ae37b56105
Ross Hadden <rosshadden@gmail.com>
2017-05-23 12:37:29 -0400

    Added mem snapshot lib.

--- src/systems/debug.cpp
+++ src/systems/debug.cpp
@@ -1,9 +1,13 @@
 #include "system.hpp"
+#include <MemoryFree.h>
 
 class DebugSystem: public System {
 	using System::System;
 
 	public:
 		void update() {
+			if (ctx->pressed("X")) {
+				Serial.println(freeMemory());
+			}
 		}
 };
