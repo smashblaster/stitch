60b8f315e232561ca0de5e6cf7053901ea095fe4
Ross Hadden <rosshadden@gmail.com>
2017-06-13 21:53:30 -0400

    Enabled cardinal system by default.

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void setup() {
 	char json[] = "{"
 		"\"angles\": \"true\","
 		"\"backdash\": \"true\","
-		"\"cardinal\": \"false\","
+		"\"cardinal\": \"true\","
 		"\"debug\": \"false\","
 		"\"remap\": \"false\","
 		"\"rumble\": \"false\""
