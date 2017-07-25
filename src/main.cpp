4832f3b9c3039822f3c27770d26e752438bc6295
Ross Hadden <rosshadden@gmail.com>
2017-07-24 20:23:28 -0400

    Squashed Stitch class into main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
 #include "Arduino.h"
-#include "stitch.cpp"
+#include "config.hpp"
+#include "context.hpp"
+#include "systems/angles.cpp"
+#include "systems/backdash.cpp"
+#include "systems/cardinal.cpp"
+#include "systems/debug.cpp"
+#include "systems/input.cpp"
+#include "systems/meta.cpp"
+#include "systems/remap.cpp"
+#include <Nintendo.h>
+#include <vector>
 
 #define pinLed LED_BUILTIN
 
-Stitch* stitch;
 const int baud = 9600;
 const int consolePin = 3;
 const int controllerPin = 4;
+
+Context* ctx;
+const Config* config;
+int step;
+
+void addSystem(char* name, System* system, bool persistent = false) {
+	ctx->addSystem(name, system, persistent, config->get(name));
+}
 
 void setup() {
 	Serial.begin(baud);
@@ -20,9 +37,54 @@ void setup() {
 		"\"remap\": \"false\","
 		"\"rumble\": \"false\""
 	"}";
-	stitch = new Stitch(consolePin, controllerPin, json);
+
+	config = new Config(json);
+	ctx = new Context(consolePin, controllerPin);
+
+	addSystem("input", new InputSystem(ctx), true);
+	addSystem("meta", new MetaSystem(ctx), true);
+
+	addSystem("remap", new RemapSystem(ctx));
+	addSystem("cardinal", new CardinalSystem(ctx));
+	addSystem("backdash", new BackdashSystem(ctx));
+	addSystem("angles", new AnglesSystem(ctx));
+	addSystem("debug", new DebugSystem(ctx));
+}
+
+void init2() {
+	for (auto &system : ctx->systems) {
+		if (system->persistent || (system->enabled && ctx->enabled)) system->init();
+	}
+	ctx->init = true;
 }
 
 void loop() {
-	stitch->update();
+	// Read from controller
+	if (!ctx->controller.read()) {
+		// Continue if no controller is found
+		ctx->init = false;
+		delay(100);
+		return;
+	}
+
+	if (!ctx->init) init2();
+
+	for (auto &system : ctx->systems) {
+		if (system->persistent || (system->enabled && ctx->enabled)) system->update();
+	}
+
+	if (ctx->isDolphin) delay(1);
+
+	step = 0;
+	while (step < ctx->stepInterval) {
+		// Write to console
+		if (!ctx->console.write(ctx->data)) {
+			ctx->init = false;
+			delay(100);
+			return;
+		}
+		step += 1;
+	}
+
+	ctx->controller.setRumble((config->get("rumble") && ctx->data.status.rumble) || ctx->rumble);
 }
